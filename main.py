e67cc299e5bc3627583cc00918419edf2dff968e
simba <simba3310@gmail.com>
2019-09-20 21:55:01 -0500

    Added Info screen widget

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager,Screen
 import os
-
 kivy.require("1.11.1")
 
 
@@ -47,18 +47,50 @@ class ConnectPage(GridLayout):
         ip = self.ip.text
         username = self.username.text
 
-        print(f"Attempting to join {ip} : {port} as {username}")
-
         with open("prev_details.txt", "w") as f:
             f.write(f"{ip},{port},{username}")
 
+            info = f"Attempting to join {ip} : {port} as {username}"
+            chat_app.info_page.update_info(info)
+            chat_app.screen_manager.current = "Info"
+
+
+class InfoPage(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 1
+        self.message = Label(halign="center", valign="middle", font_size="35")
+        self.message.bind(width=self.update_text_width)
+        self.add_widget(self.message)
+
+    def update_info(self,message):
+        self.message.text = message
+
+    def update_text_width(self, *_):
+        self.message.text_size = (self.message.width*0.9, None)
+
 
 class MainApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.screen_manager = ScreenManager()
+        self.connect_page = ConnectPage()
+        self.info_page = InfoPage()
+
     def build(self):
-        return ConnectPage()
+        screen = Screen(name="Connect")
+        screen.add_widget(self.connect_page)
+        self.screen_manager.add_widget(screen)
+
+        screen = Screen(name="Info")
+        screen.add_widget(self.info_page)
+        self.screen_manager.add_widget(screen)
+
+        return self.screen_manager
 
 
 if __name__ == "__main__":
-    MainApp().run()
+    chat_app = MainApp()
+    chat_app.run()
 
 
